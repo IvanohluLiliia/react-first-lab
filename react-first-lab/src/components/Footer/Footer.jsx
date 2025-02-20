a63ef53c11ad855a4bbee9bc9b935f3f44d91432
IvanohluLiliia <liliiaivanohlu@gmail.com>
2025-02-20 20:24:11 +0200

    Lab11_Ivanohlu

---
+++ react-first-lab/src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <h2>Smile!😊</h2>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ react-first-lab/src/components/Main/Main.jsx
@@ -0,0 +1,13 @@
+import "./Main.css";
+
+function Main() {
+  return (
+    <main className="main">
+      <h1>Ласкаво просимо!</h1>
+      <p>Це головний розділ сайту. Натисніть кнопку, щоб дізнатися більше.</p>
+      <button className="main-button">Дізнатися більше</button>
+    </main>
+  );
+}
+
+export default Main;
